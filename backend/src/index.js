e78e16faeb92292b1ee632bca1093d6102d88206
berrelleza75 <jberrelleza75@gmail.com>
2026-03-23 23:58:23 -0700

    update backend desplegado

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://rise-connect-crm.vercel.app'
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
